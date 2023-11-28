ba017bd123a436175ee15773a3726648da421004
Joshua King <joshua-coding@Joshuas-MacBook-Pro.local>
2023-11-28 14:37:04 -0700

    final touches of code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -5,20 +5,6 @@ import { header } from './header';
 const initdb = async () => {
   console.log('Initialize the database');
 
-openDB('jate', 1, {
-  upgrade(db) {
-    if (db.objectStoreNames.contains('jate')) {
-      console.log('jate database already exists');
-      return;
-    }
-    db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-    console.log('jate database created');
-    seedDb();
-    console.log('jate database seeded');
-  },
-});
-};
-
 // Define function to seed the db with the header
 const seedDb = async () => {
   console.log('Update the database');
@@ -40,6 +26,20 @@ const seedDb = async () => {
   console.log('jate database seeded');
 
   return result;
+};
+
+openDB('jate', 1, {
+  upgrade(db) {
+    if (db.objectStoreNames.contains('jate')) {
+      console.log('jate database already exists');
+      return;
+    }
+    db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+    console.log('jate database created');
+    seedDb();
+    console.log('jate database seeded');
+  },
+});
 };
 
   // Seed local storage if empty
